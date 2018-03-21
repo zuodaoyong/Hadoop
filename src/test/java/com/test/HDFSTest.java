85a4bedfc2ef4a4222d6dc796fc6dc7adb110713
zdy48195 <zdy48195@ly.com>
2018-03-21 09:55:13 +0800

    hdfs

--- src/test/java/com/test/HDFSTest.java
+++ src/test/java/com/test/HDFSTest.java
@@ -1,6 +1,6 @@
 package com.test;
 
-import java.io.IOException;
+import java.net.URL;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -10,10 +10,19 @@ public class HDFSTest {
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf=new Configuration();
-		conf.set("fs.defaultFS","hdfs://192.168.15.129:8020");
+		conf.set("fs.defaultFS","hdfs://192.168.237.129:8020");
 		FileSystem fs=FileSystem.get(conf);
-		boolean bool = fs.createNewFile(new Path("/test/test1.txt"));
-	    System.out.println(bool);
+		//fs.mkdirs(new Path("/test"));
+		//boolean bool = fs.createNewFile(new Path("/test/test1.txt"));
+	    //System.out.println(bool);
+		uploadFile(fs);
 	    fs.close();
+	}
+	
+	@SuppressWarnings("static-access")
+	public static void uploadFile(FileSystem fs) throws Exception{
+		URL url = HDFSTest.class.getClassLoader().getSystemResource("excels/11.xlsx");
+		Path src=new Path(url.toURI());
+		fs.copyFromLocalFile(src, new Path("/test/11.xlsx"));
 	}
 }
