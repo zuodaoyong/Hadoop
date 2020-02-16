125bfb38f683e19269e627dfdf66b3765550a54c
neo <1525714537@qq.com>
2020-02-16 09:46:09 +0800

    pom的log修改

--- src/test/java/com/mapReduce/TestDriver.java
+++ src/test/java/com/mapReduce/TestDriver.java
@@ -2,17 +2,24 @@ package com.mapReduce;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class TestDriver {
+public class TestDriver implements Tool {
+    Configuration configuration=null;
     public static void main(String[] args) throws Exception{
+        int run = ToolRunner.run(new TestDriver(), args);
+        System.exit(run);
+    }
 
+
+    @Override
+    public int run(String[] strings) throws Exception {
         //获取配置信息，job对象实例
         Configuration configuration=new Configuration();
         configuration.set(KeyValueLineRecordReader.KEY_VALUE_SEPERATOR,"\t");
@@ -22,19 +29,27 @@ public class TestDriver {
         job.setJarByClass(TestDriver.class);
         //指定本业务job要使用的mapper/Reducer业务类
         job.setMapperClass(TestMapper.class);
-        job.setReducerClass(TestReducer.class);
         //指定mapper输出数据的kv类型
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Flow.class);
-        //指定最终输出的数据的kv类型
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Flow.class);
+        job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
         //指定job的输入原始文件所在目录
-        FileInputFormat.setInputPaths(job, new Path("\\mapreduce\\flow\\phoneflow"));
-        FileOutputFormat.setOutputPath(job,new Path("\\mapreduce\\flow\\output"));
+        FileInputFormat.setInputPaths(job, new Path("/test/video"));
+        FileOutputFormat.setOutputPath(job,new Path("/test/output"));
         //将job中配置的相关参数，以及job所用的java类所在的jar包， 提交给yarn去运行
         boolean result = job.waitForCompletion(true);
-        System.exit(result ? 0 : 1);
+        //System.exit(result ? 0 : 1);
+        return result ? 0 : 1;
+    }
 
+    @Override
+    public void setConf(Configuration configuration) {
+        configuration=configuration;
+    }
+
+    @Override
+    public Configuration getConf() {
+        return configuration;
     }
 }
--- src/test/java/com/mapReduce/TestMapper.java
+++ src/test/java/com/mapReduce/TestMapper.java
@@ -1,21 +1,45 @@
 package com.mapReduce;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class TestMapper extends Mapper<LongWritable, Text,Text,Flow> {
+public class TestMapper extends Mapper<LongWritable, Text, NullWritable,Text> {
 
     @Override
     protected void map(LongWritable key, Text line, Context context) throws IOException, InterruptedException {
-        Flow flow=new Flow();
-        String[] split = line.toString().split("\t");
-        String phoneNum = split[1];
-        flow.setUpFlow(Long.valueOf(split[split.length-3]));
-        flow.setDownFlow(Long.valueOf(split[split.length-2]));
-        flow.setSumFlow(flow.getUpFlow()+flow.getDownFlow());
-        context.write(new Text(phoneNum),flow);
+        String etlString = oriString2ETLString(line.toString());
+        if(StringUtils.isBlank(etlString))
+            return;
+        context.write(NullWritable.get(),new Text(etlString));
+    }
+
+
+    public static String oriString2ETLString(String ori){
+        StringBuilder etlString = new StringBuilder();
+        String[] splits = ori.split("\t");
+        if(splits.length < 9)
+            return null;
+        splits[3] = splits[3].replace(" ", "");
+        for(int i = 0; i < splits.length; i++){
+            if(i < 9){
+                if(i == splits.length - 1){
+                    etlString.append(splits[i]);
+                }else{
+                    etlString.append(splits[i] + "\t");
+                }
+            }else{
+                if(i == splits.length - 1){
+                    etlString.append(splits[i]);
+                }else{
+                    etlString.append(splits[i] + "&");
+                }
+            }
+        }
+        return etlString.toString();
     }
 }
